ca3d1e09e17f696e1df85cdb1eb8047719374226
anton <antonananiev@gmail.com>
2025-04-29 12:40:18 +0700

    groups page

--- src/test/java/pages/GroupsPage.java
+++ src/test/java/pages/GroupsPage.java
@@ -1,6 +1,7 @@
 package pages;
 
 import common.BasePage;
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
 public class GroupsPage extends BasePage {
@@ -9,7 +10,10 @@ public class GroupsPage extends BasePage {
     By addButton = By.name("Add");
     By groupName = By.xpath("//XCUIElementTypeStaticText[@name=\"Group name\"]/..");
     By groupDescription= By.xpath("//XCUIElementTypeStaticText[@name=\"Group description\"]/..");
+    By pribateBtn= By.xpath("//XCUIElementTypeSwitch[@name=\"Private\"]");
 
+
+    @Step("Создание новой Не приватной группы")
     public void createGroup() {
         String group = "Group_"+getRandomNumber(999999);
         profile.clickGroups();
@@ -23,4 +27,40 @@ public class GroupsPage extends BasePage {
         click(elementName("Create"));
         waitElementName(group);
     }
+
+    @Step("Создание новой Приватной группы")
+    public void createPrivateGroup() {
+        String group = "Group_"+getRandomNumber(999999);
+        profile.clickGroups();
+        click(addButton);
+        waitElementName("New Group");
+        click(elementName("Add photo"));
+        click(elementName("ic_gray_unselect_checkbox"));
+        click(elementName("Done"));
+        setText(groupName, group);
+        setText(groupDescription, "description");
+        click(pribateBtn);
+        click(elementName("Create"));
+        waitElementName(group);
+    }
+//    @Step("Редактирование группы '{groupName}'")
+//    public void editGroup() throws InterruptedException {
+//        String originalGroupName = "Group_" + getRandomNumber(999999);
+//        String newGroupName = "Test group_" + getRandomNumber(999999999);
+//
+//        click(By.xpath("//XCUIElementTypeStaticText[@name='" + originalGroupName + "']"));
+//        wait(1);
+//        click(elementName("Edit group"));
+//        clearAndSendKeys(By.id(originalGroupName), newGroupName);
+//        clearAndSendKeys(groupDescription, getRandomText(250));
+//        click(elementName("Save"));
+//        wait(1);
+//        clickButton("Back");
+//        waitElement(By.xpath("//XCUIElementTypeStaticText[@name='" + newGroupName + "']"));
+//    }
+//
+//    @Step("Нажать на кнопку редактирования группы")
+//    public void clickEditGroup() {
+//        click(elementName("Edit Group"));
+//    }
 }
--- src/test/java/tests/GroupsTest.java
+++ src/test/java/tests/GroupsTest.java
@@ -2,14 +2,22 @@ package tests;
 
 import common.BasePage;
 import io.qameta.allure.Description;
+import io.qameta.allure.Step;
 import org.testng.annotations.Test;
 
 public class GroupsTest extends BasePage {
 
     @Test(description = "Group lifecycle")
-    @Description("Group lifecycle")
-    public void groupLifecycle() {
+    @Description("Создание Не приватной группы")
+    public void createPublicGroup() {
         profile.openProfile();
         groups.createGroup();
+    }
+
+    @Test(description = "Создание Приватной группы")
+    @Description("Создание Приватной группы")
+    public void createPrivateGroup() {
+        profile.openProfile();
+        groups.createPrivateGroup();
     }
 }
